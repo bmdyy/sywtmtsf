020d8f2e63666f983fee588cd3201f0c034ab774
William Moody <w.moody@secturo.com>
2025-12-01 01:04:31 +0100

    Began Generate Maps script

---
+++ Scripts/GenerateMaps.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+import requests
+
+# https://api.mapbox.com/styles/v1/mapbox/light-v11/static/[-122.5295,37.6933,-122.346,37.8384]/700x700?access_token=pk.eyJ1IjoibGlmZXRpbWVraWRuZXkiLCJhIjoiY21pbWJ5YXBsMWN5bjNrcTcwaDNkcmJ6NCJ9.S7xI5KnBJszSZhQO8eZjGw
+
